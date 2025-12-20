b60550e68470c6e3750ef11e30a871af12781a21
JalalLinuX <m.jalalzadeh@webnegah.com>
2025-12-20 13:35:12 +0330

    register commands

--- app/Providers/GeneralServiceProvider.php
+++ app/Providers/GeneralServiceProvider.php
@@ -4,6 +4,7 @@ namespace Modules\General\Providers;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Modules\General\Console\Command\InstallCommand;
 use Nwidart\Modules\Traits\PathNamespace;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
@@ -43,7 +44,9 @@ class GeneralServiceProvider extends ServiceProvider
      */
     protected function registerCommands(): void
     {
-        // $this->commands([]);
+         $this->commands([
+             InstallCommand::class,
+         ]);
     }
 
     /**
